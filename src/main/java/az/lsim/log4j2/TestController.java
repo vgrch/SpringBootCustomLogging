f668e279ea469a5d5abeb094347699c5bf04356e
vgrch <vgr.asadov@gmail.com>
2018-05-25 13:53:34 +0400

    log4j2.properties added

--- src/main/java/az/lsim/log4j2/TestController.java
+++ src/main/java/az/lsim/log4j2/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class TestController {
 
-    Logger logger = LogManager.getLogger(TestController.class);
+    static Logger logger = LogManager.getLogger(TestController.class);
 
     @RequestMapping("/test")
     public @ResponseBody String test(){
@@ -20,5 +20,15 @@ public class TestController {
         logger.debug("debug message");
         logger.fatal("FATAL message");
         return "Log4j2 tested; Look at console";
+    }
+
+
+    public static void main(String[] args) {
+        logger.warn("warn message");
+        logger.info("info message");
+        logger.trace("trace message");
+        logger.error("error message");
+        logger.debug("debug message");
+        logger.fatal("FATAL message");
     }
 }
